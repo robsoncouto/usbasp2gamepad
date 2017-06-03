47b07c5b19161d56b7dcf131b0f2969b632f8ad0
Robson <robsoncouto@outlook.com>
2017-06-03 13:05:34 -0300

    Added usbasp defines file

---
+++ usbasp.h
@@ -0,0 +1,19 @@
+
+#ifndef _USBASP_H
+#define _USBASP_H
+
+//On PORTB
+#define MISO 4
+#define SCK  5
+#define MOSI 3
+#define RESET 2
+//On PORTD
+#define RX 0
+#define TX 1
+
+//On PORTC, active low
+#define GREEN_LED 1
+#define RED_LED 0
+#define SLOW_SCK 2
+
+#endif
